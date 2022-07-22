c162a400ab7e6aa17602e243cfe19559f979808f
kerimartin <keri.martin59@gmail.com>
2022-07-22 14:47:03 +0300

    Added Startup cs

---
+++ DonetForGit/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+
+namespace ReadHeaderMVC
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseAuthentication();
+        }
+
+    }
+}
